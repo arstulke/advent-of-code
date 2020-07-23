29c1fe28ce550e07dbc20abb9b3ecbda071787c6
Arne Stulken <arne.stulken@btc-ag.com>
2020-07-23 12:19:55 +0200

    [puzzle] moving getAllNumericCombinations to utils

--- src/puzzle/2015-12-09.ts
+++ src/puzzle/2015-12-09.ts
@@ -1,4 +1,5 @@
 import { readInput } from '@/util/file-utils';
+import { getAllNumericCombinations } from "@/util/combination.util";
 
 interface SingleRoute {
     a: string;
@@ -13,42 +14,6 @@ interface CombinedRoute {
 
 let locationColWidth;
 let distanceColWidth;
-
-function getAllNumericCombinations(itemCount: number): number[][] {
-    function getInheritedCombinations(rootCombination: number[], possibleValues: number[]): number[][] {
-        const unflatArr = possibleValues.map((pv, index) => {
-            const possibleValuesNew = possibleValues.slice();
-            possibleValuesNew.splice(index, 1);
-
-            const combination = rootCombination.slice();
-            combination.push(pv);
-
-            if (possibleValuesNew.length > 0) {
-                return getInheritedCombinations(combination, possibleValuesNew);
-            } else {
-                return [combination];
-            }
-        });
-
-        const flatArr = [];
-        for (const combinations of unflatArr) {
-            for (const combination of combinations) {
-                flatArr.push(combination);
-            }
-        }
-
-        return flatArr;
-    }
-
-    const possibleValues: number[] = (() => {
-        const arr = [];
-        for (let i = 0; i < itemCount; i++) {
-            arr.push(i);
-        }
-        return arr;
-    })();
-    return getInheritedCombinations([], possibleValues);
-}
 
 function getDistance(locA: string, locB: string, singleRoutes: SingleRoute[]): number {
     const routes = singleRoutes.filter(r => (r.a === locA && r.b === locB) || (r.a === locB && r.b === locA));
